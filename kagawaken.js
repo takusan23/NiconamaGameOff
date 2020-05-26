b3e287f0895e294db251ccf75e3093ba399f367f
takusan23 <takumu1200@gmail.com>
2020-05-26 17:50:52 +0900

    未ログイン時動かなかったの直した

--- kagawaken.js
+++ kagawaken.js
@@ -14,16 +14,22 @@ window.onload = () => {
         // ゲーム・ギフト・エモーション演出を非表示にする の要素
         const niconamaGameOffSwitch = document.getElementsByClassName('___target___1Oicc ___target___21D1y')[4]
         // 演出非表示OFFのときだけONにする
-        if (niconamaGameOffSwitch.getAttribute('aria-pressed') === 'false') {
+        if (niconamaGameOffSwitch !== undefined && niconamaGameOffSwitch.getAttribute('aria-pressed') === 'false') {
             // 押す
             niconamaGameOffSwitch.click()
             // 結果
             console.log(`ニコ生ゲームを無効にする：${niconamaGameOffSwitch.getAttribute('aria-pressed')}`)
+        } else {
+            // 無いとき
+            const notLoginGameOffSwitch = document.getElementsByClassName('___target___1Oicc ___target___21D1y')[3]
+            notLoginGameOffSwitch.click()
+            // 結果
+            console.log(`未ログイン時でニコ生ゲームを無効にする：${notLoginGameOffSwitch.getAttribute('aria-pressed')}`)
         }
         // 閉じるボタンを押す
         const closeMoreSettingPanel = document.getElementsByClassName('___close-button___23Wwj ___close-button___fQHeK')[0]
         closeMoreSettingPanel.click()
-    }, 100)
+    }, 500)
     // あと「エモーションボタン」を非表示にする
     const emotionButton = document.getElementsByClassName('___emotion-button___1Rolf ___button___Re8M9')[0]
     emotionButton.style.display = 'none'
